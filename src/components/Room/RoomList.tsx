7e54dea25bd149cd9ce1bca30724ba3c1a1c8f7c
Mount <simon@mount.agency>
2024-08-08 11:22:04 +0200

    fix: spelling

--- src/components/Room/RoomList.tsx
+++ src/components/Room/RoomList.tsx
@@ -47,7 +47,7 @@ export default function RoomList({ initialRooms }: ClientRoomListProps) {
   if (userRooms.length === 0) {
     return (
       <div className="text-center text-sm text-neutral-500">
-        No rooms joined yet.
+        No lobbies yet.
       </div>
     );
   }
